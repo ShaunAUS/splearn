5c8c689336dcb384a9d9cfaa6b0d7c6dda425085
minjae <wnsqud70@gmail.com>
2025-08-02 23:40:44 +0900

    chore () : 주석

--- src/main/java/tobyspring/splearn/domain/PasswordEncoder.java
+++ src/main/java/tobyspring/splearn/domain/PasswordEncoder.java
@@ -1,6 +1,7 @@
 package tobyspring.splearn.domain;
 
 //도메인에서 사용하는 서비스
+// 논리상 application-required 패키지에 위치해야 하지만, 그렇게 되면 의존성 방향이 맞지않음
 public interface PasswordEncoder {
     String encode(String password);
     boolean matches(String password, String passwordHash);
