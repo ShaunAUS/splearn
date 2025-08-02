995f9a10736d54702e0290dfd22e944f427460c1
minjae <wnsqud70@gmail.com>
2025-08-02 23:38:06 +0900

    chore () : 주석

--- src/main/java/tobyspring/splearn/application/required/MemberRepository.java
+++ src/main/java/tobyspring/splearn/application/required/MemberRepository.java
@@ -3,6 +3,7 @@ package tobyspring.splearn.application.required;
 import org.springframework.data.repository.Repository;
 import tobyspring.splearn.domain.Member;
 
+// jpa가 제공하는 repository 상속받아도 괜찮다.
 public interface MemberRepository extends Repository<Member, Long> {
 
     Member save(Member member);
--- src/main/java/tobyspring/splearn/domain/package-info.java
+++ src/main/java/tobyspring/splearn/domain/package-info.java
@@ -1,3 +1,4 @@
+// 이 package의 모든 메소드의 파라미터와 return 값은 null이 될 수 없음을 명시
 @NonNullApi
 package tobyspring.splearn.domain;
 
